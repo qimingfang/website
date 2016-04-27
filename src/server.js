cb3c7faaddbc9987779556bee496848247d46e19
Qiming Fang <qf26@cornell.edu>
2016-04-27 14:17:19 -0700

    serve public into / instead of /public

--- src/server.js
+++ src/server.js
@@ -4,10 +4,10 @@ import express from 'express'
 export const app = express()
 
 // view engine setup
-app.use('/public', express.static(path.join(__dirname, '/public')))
+app.use('/', express.static(path.join(__dirname, '/public')))
 
 const address = process.env.OPENSHIFT_NODEJS_IP || 'localhost'
-const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 5000
+const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3000
 
 app.listen(port, address, (err) => {
   if (err) {
