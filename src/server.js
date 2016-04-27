2ba44cc39774a24c84ed2951a8102a2c1bea5915
Qiming Fang <qf26@cornell.edu>
2016-04-27 14:08:35 -0700

    update openshift params

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ export const app = express()
 
 // view engine setup
 app.use('/public', express.static(path.join(__dirname, '/../public')))
-app.listen((process.env.PORT || 5000), (err) => {
+app.listen((process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 5000), (err) => {
   if (err) {
     return console.log(err)
   }
